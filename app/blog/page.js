8f1ef42861dafea72696479b7e252961c1fb5795
Bala-Saravanan <jbalasaravanan174@gmail.com>
2025-07-05 16:35:18 +0530

    server url

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,12 +1,12 @@
-// "use client";
-
 import BlogGrid from "@/components/BlogGrid";
-// import { useEffect, useState } from "react";
 
 async function getAllBlogs(page) {
-  const res = await fetch(`http://localhost:3000/api/blog/get?page=${page}`, {
-    cache: "no-cache",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_API_URL}/api/blog/get?page=${page}`,
+    {
+      cache: "no-cache",
+    }
+  );
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
